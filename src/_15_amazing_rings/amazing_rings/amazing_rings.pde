06e85cc740b55cfc9b657aa51b27613e08937000
League Student <league@iMac-21.attlocal.net>
2020-02-14 17:46:23 -0800

    lol2 and 1

--- src/_15_amazing_rings/amazing_rings/amazing_rings.pde
+++ src/_15_amazing_rings/amazing_rings/amazing_rings.pde
@@ -1,18 +1,44 @@
-
-
+int s1 = 3;
+int s2 = 3;
+int x1 = 450;
+int x2 = 1350;
 void setup() {
-  size(1600, 900);
+  size(1800, 900);
 }
 void draw(){
+  background(255,255,255);
   lol1();
-  
+  lol2();
 }
 
 void lol1(){
-for (int i=600; i>0; i--) {
+for (int i=900; i>0; i--) {
+  
     stroke(0.1);
-    ellipse(450,450,i,i);
-    noFill();
+      noFill();
+    ellipse(x1,450,i,i);
+  
     i-=16;
+    
   }
+  x1+=s1;
+   if(x1>1800 || x1<0){
+     s1=-s1;
+     
+    }
+}
+void lol2(){
+for (int e=900; e>0; e--) {
+  
+    stroke(0.1);
+      noFill();
+    ellipse(x2,450,e,e);
+  
+    e-=16;
+   
+  }
+  x2-=s2;
+   if(x2<0 || x2> 1800){
+     s2=-s2;
+    }
 }
--- src/_16_obedient_robot/ObedientRobot.java
+++ src/_16_obedient_robot/ObedientRobot.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
-	 Robot rob = new Robot();
+	  Robot rob = new Robot();
 	 
 	
 	
@@ -40,24 +40,39 @@ public class ObedientRobot {
 	}
 	public static void main(String[] args) {
 		ObedientRobot rob20 = new ObedientRobot();	
+			rob20.start();	
+		
+		}
+	void start() {
 		String drawBoi = JOptionPane.showInputDialog(null, "What do you want to draw? (Square, Circle, Triangle)");
-		String colorBoi = JOptionPane.showInputDialog(null,"What color do you want it to be? (Blue, Green, Red, Orange,Purple");
+		String colorBoi = JOptionPane.showInputDialog(null,"What color do you want it to be? (Blue, Green, Red, Orange,");
 		
 		if(colorBoi.equalsIgnoreCase("Blue")) {
-		
+		rob.setPenColor(20,20,255);
+		}else if(colorBoi.equalsIgnoreCase("Green")) {
+		rob.setPenColor(20,255,20);
+		}else if(colorBoi.equalsIgnoreCase("Red")) {
+		rob.setPenColor(255,20,20);
+		}else if(colorBoi.equalsIgnoreCase("Yellow")){
+		rob.setPenColor(255,255,0);
+		}else {
+		rob.setPenColor(20,20,20);
 		}
 		
 		
 		if(drawBoi.equalsIgnoreCase("Square")) {
-			rob20.drawSquare();
+			             drawSquare();
 		}else if(drawBoi.equalsIgnoreCase("Circle")) {
-			rob20.drawCircle();
+			drawCircle();
 		}else {
-			rob20.drawTriangle();
+			drawTriangle();
 		}
 		
+
 		
 		
-		}
+		
+		
+	}
 }
      
